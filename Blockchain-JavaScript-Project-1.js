635a594cd9b4470c9053cc0d40620ede110684fa
Iris-JQ-Chen <2906514421@qq.com>
2021-03-08 21:56:03 +0800

    运行上述代码，观察对一个字符串进行SHA-256、RipeMD160、hash256和hash160的结果。

--- Blockchain-JavaScript-Project-1.js
+++ Blockchain-JavaScript-Project-1.js
@@ -1,25 +1,23 @@
-const 
-	bitcoin = require('bitcoinjs-lib'),
-	createHash = require('create-hash');
+const bitcoin = require('bitcoinjs-lib'), createHash = require('create-hash');	//能用
 
-function standardHash(name,data){
-	let h = createHash(name);
-	return h.update(data).digest();
+function standardHash(name, data) {
+    let h = createHash(name);
+    return h.update(data).digest();
 }
 
-function hash160(data){
-	let h1 = standardHash('sha256', data);
-	let h2 = standardHash('repemd160',h1);
-	return h2;
+function hash160(data) {
+    let h1 = standardHash('sha256', data);
+    let h2 = standardHash('ripemd160', h1);
+    return h2;
 }
 
-function hash256(data){
-	let h1 = standardHash('sha256',data);
-	let h2 = standardHash('sha256',h1);
-	return h2;
+function hash256(data) {
+    let h1 = standardHash('sha256', data);
+    let h2 = standardHash('sha256', h1);
+    return h2;
 }
 
-let s = 'bitcoin is  awesome';
+let s = 'bitcoin is awesome';
 console.log('ripemd160 = ' + standardHash('ripemd160', s).toString('hex'));
 console.log('  hash160 = ' + hash160(s).toString('hex'));
 console.log('   sha256 = ' + standardHash('sha256', s).toString('hex'));
