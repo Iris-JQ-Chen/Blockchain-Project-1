11ea253acca0455563ec4ff413ec0fbb0fd2c678
Iris-JQ-Chen <2906514421@qq.com>
2021-03-08 19:22:54 +0800

    function standardHasd

--- Blockchain-JavaScript-Project-1.js
+++ Blockchain-JavaScript-Project-1.js
@@ -1,3 +1,8 @@
 const 
 	bitcoin = require('bitcoinjs-lib'),
 	createHash = require('create-hash');
+
+function standardHash(name,data){
+	let h = createHash(name);
+	return h.update(data).digest();
+}
